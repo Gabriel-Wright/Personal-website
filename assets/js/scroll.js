86c8c94d8dc5385092a670a088b2db48885f033e
Gabriel <gabemagwright@gmail.com>
2025-08-14 17:49:05 +0100

    finishing header - link on GABRIEL WRIGHT

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,8 @@
 document.addEventListener('scroll', changeHeaderStyle);
 
 function changeHeaderStyle() {
+    //Could throttle this function for performance,
+    //but OK for now
     const header = document.querySelector('header');
     if (window.scrollY > 10) {
         header.classList.add('scrolled');
